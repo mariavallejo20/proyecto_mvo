e4c5f80db73a6bd802a2730fa35b6fd2ce3c84b5
martahuetem <martahuete46@gmail.com>
2022-10-31 14:00:29 +0100

    Modificación del fichero usuario.js. Closes #17

--- src/usuario.js
+++ src/usuario.js
@@ -1,13 +1,27 @@
 class Usuario{
     /**
+     * Esta clase contendrá toda la información del usuario. 
+     * Se podraá identificar con el email.
+     * Tendremos la lista de los ingredientes de los que dispone.
+     * Tendremos el tiempo del que dispone.
+     * Tendremos la lista de la colección de recetas que se adecuan a sus neceesidades.
+     */
+
+    /**
      * Constructor de la clase Usuario
      * @param {String} nombre
      * @param {String} email
+     * @param {Array} ingredientes_disponibles
+     * @param {int} tiempo_disponible
+     * @param {Array} recetas_propuestas
      */
 
-    constructor(nombre, email){
+    constructor(nombre, email, ingredientes_disponibles, tiempo_disponible, recetas_propuestas){
         this.nombre = nombre;
         this.email = email
+        this.ingredientes_disponibles = ingredientes_disponibles;
+        this.tiempo_disponible = tiempo_disponible;
+        this.recetas_propuestas = recetas_propuestas;
     }
 
     /**
@@ -24,6 +38,30 @@ class Usuario{
      */
      getEmail(){
         return this.email;
+    }
+
+    /**
+     * Método para obtener los ingredientes disponibles
+     * @returns {Array}
+     */
+     getIngredientesDisponibles(){
+        return this.ingredientes_disponibles;
+    }
+
+    /**
+     * Método para obtener el tiempo disponible
+     * @returns {int}
+     */
+     getTiempoDisponible(){
+        return this.tiempo_disponible;
+    }
+
+    /**
+     * Método para obtener la lista de recetas propuestas
+     * @returns {Array}
+     */
+     getRecetasPropuestas(){
+        return this.recetas_propuestas;
     }
 }
 
