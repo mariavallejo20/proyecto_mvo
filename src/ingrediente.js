57bd3cf5985f5ca66b0f9deb05b21e1fbff71d0a
mariavallejo20 <mariavallejo20@correo.ugr.es>
2022-11-24 13:44:07 +0100

    closes #34 elimino detalle de tiempo de cada ingrediente

--- src/ingrediente.js
+++ src/ingrediente.js
@@ -6,18 +6,12 @@ class Ingrediente{
     /**
      * Constructor de la clase Ingrediente
      * @param {String} nombre // nombre del ingrediente
-     * @param {int} id_ingrediente // ID con el que se va a identificar al ingrediente (Resuelve el problema 2)
-     * @param {int} tiempo_cocer //Tiempo que se tarda en cocer el ingrediente (Resuelven el problema 1)
-     * @param {int} tiempo_frito //Tiempo que se tarda en freir el ingrediente
-     * @param {int} tiempo_plancha //Tiempo que se tarda en hacer en la plancha el ingrediente
+     * @param {int} id_ingrediente // ID con el que se va a identificar al ingrediente
      */
 
     constructor(nombre, id_ingrediente, tiempo_cocer, tiempo_frito, tiempo_plancha){
         this.nombre = nombre;
         this.id_ingrediente = id_ingrediente;
-        this.tiempo_cocer = tiempo_cocer;
-        this.tiempo_frito = tiempo_frito;
-        this.tiempo_plancha = tiempo_plancha;
     }
 
     /**
@@ -34,30 +28,6 @@ class Ingrediente{
      */
      getId(){
         return this.id_ingrediente;
-    }
-
-    /**
-     * Método para obtener el tiempo en el que se tarda en cocer dicho ingrediente
-     * @returns {int}
-     */
-     getTiempoCocer(){
-        return this.tiempo_cocer;
-    }
-
-    /**
-     * Método para obtener el tiempo en el que se tarda en freir dicho ingrediente
-     * @returns {int}
-     */
-     getTiempoFreir(){
-        return this.tiempo_frito;
-    }
-
-    /**
-     * Método para obtener el tiempo en el que se tarda en hacer en la plancha dicho ingrediente
-     * @returns {int}
-     */
-     getTiempoPlancha(){
-        return this.tiempo_plancha;
     }
 }
 
