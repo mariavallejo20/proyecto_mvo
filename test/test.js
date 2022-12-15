abf612c8b6d13b741ea5a3b5d5ddf7c2638b049e
mariavallejo20 <mariavallejo20@correo.ugr.es>
2022-12-15 11:59:09 +0100

    avanzo #37 elimino tests no relacionado con la lógica de negocio

--- test/test.js
+++ test/test.js
@@ -5,32 +5,6 @@ import Receta from '../src/receta.js'
 // Testeamos la clase Usuario
 describe ('Clase Usuario', function(){
 
-    // Testea que se haya cargado bien la biblioteca
-    describe('Carga de la biblioteca Usuario', function(){
-
-        // Obtenemos un error si no se ha cargado correctamente la biblioteca Usuario
-        it('Se ha cargado correctamente', function(){
-            assert.ok(Usuario, "Cargado");
-        });
-    });
-
-    // Testeamos que el nombre del usuario NO sea una cadena vacía
-    describe('Nombre usuario', function(){
-
-        // Obtendremos un error si la cadena del nombre NO es superior a 0
-        it('El nombre del usuario NO puede ser una cadena vacía', function(){
-            //Ajustar
-            var nombre = 'Maria Vallejo';
-            
-            //Actuar
-            var nuevo_usuario = new Usuario(nombre, null, null);
-
-            //Afirmar
-            assert.isAbove(nuevo_usuario.getNombre().length, 0);
-            
-        });
-    });
-
     // Testeamos la lógica de negocio
     describe('Recetas propuestas', function(){
 
